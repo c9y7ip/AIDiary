43bdbce9b54d385047949c0a185aba370a3e4c8b
Raymond Yip <c9y7ip@gmail.com>
2024-05-03 14:43:22 -0700

    First upload

--- main.ts
+++ main.ts
@@ -1,134 +1,131 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Editor, PluginSettingTab, App,Setting,Notice  } from "obsidian";
+import { OpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import systemPrompt from "prompt";
+import * as dotenv from 'dotenv';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+
+interface DiarySetting {
+	OpenAIKey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: Partial<DiarySetting> = {
+	OpenAIKey: "",
+};
+
+export class DiarySettingTab extends PluginSettingTab {
+	plugin: OpenAIDiaryPlugin;
+  
+	constructor(app: App, plugin: OpenAIDiaryPlugin) {
+	  super(app, plugin);
+	  this.plugin = plugin;
+	}
+  
+	display(): void {
+	  let { containerEl } = this;
+  
+	  containerEl.empty();
+  
+	  new Setting(containerEl)
+		.setName("OpenAI Key")
+		.setDesc("User OpenAI Key")
+		.addText((text) =>
+		  text
+			.setPlaceholder("<openai-key>")
+			.setValue(this.plugin.settings.OpenAIKey)
+			.onChange(async (value) => {
+			  console.log(value)
+			  this.plugin.settings.OpenAIKey = value
+			  await this.plugin.saveSettings();
+			})
+		);
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        OPEN_AI_KEY: string;
+      }
+    }
+  }
+  
+export default class OpenAIDiaryPlugin extends Plugin {
+	settings: DiarySetting;
 
-	async onload() {
+
+    async onload() {
+
 		await this.loadSettings();
+		this.addSettingTab(new DiarySettingTab(this.app, this));
+		dotenv.config();
+		
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const llm = new OpenAI({
+            model: "gpt-3.5-turbo",
+            openAIApiKey: process.env.OPEN_AI_KEY,
+            temperature: 1.0 
+        });
+		
+		this.addRibbonIcon("dice", "Print to console", async () => {
+				const activeEditor = this.app.workspace.activeEditor
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+				const prompt = PromptTemplate.fromTemplate(systemPrompt);
+				
+				const formattedPrompt = await prompt.format({
+					text: activeEditor?.editor?.getValue(),
+				});
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				new Notice('[AI] I am reading ...');
+				await llm.invoke(formattedPrompt).then(res=>{
+					console.log(res)
+					activeEditor?.editor.replaceRange(
+						res,
+						activeEditor?.editor.getCursor()
+					);
+					new Notice('[AI] Finish !');
+				});
+		  });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		  
+        this.addCommand({
+            id: "Load OpenAI Diary",
+            name: "Load OpenAI Diary",
+            editorCallback: async (editor: Editor) => {
+                const user_input = editor.getValue()
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+                    const prompt = PromptTemplate.fromTemplate(systemPrompt);
+                
+                    const formattedPrompt = await prompt.format({
+                        text: user_input,
+                    });
+					new Notice('[AI] I am reading ...');
+                    await llm.invoke(formattedPrompt).then(res=>{
+                        console.log(res)
+                        editor.replaceRange(
+                            res,
+                            editor.getCursor()
+                        );
+						new Notice('[AI] Finish !');
+                    });
+            }
+        });
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
+    async onunload() {
+        // Your cleanup code here, if any
+    }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
+	  }
+	
 	async saveSettings() {
-		await this.saveData(this.settings);
+	await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
---
+++ src/prompt.md
@@ -0,0 +1,35 @@
+`{text}`
+Perform the following actions base on the content delimitied by ``` :
+
+1. Collect all my thoughts and notes of the day and rewrite them in the first-person narrative to create a complete version of my diary.
+2. The new version of the diary should have improved logical structure and writing quality, but please do not change the original meaning of my diary.
+3. After the new version of the diary, please summarize the key takeaways and the things I am grateful for from my day in bullet points to let me know what I have learned today.
+4. Based on the diary, provide me some respone and feedback act as a character describe in the #Role.
+5. Please remember to communicate with me using by 繁體中文 and Taiwanese terms.
+
+### Output
+
+Please follow the following format delimitied by triple quotes
+
+"""
+
+### Today's date in the foramt yyyy/mm/dd - title of today diary
+
+[The rewritien version that generated from tasks 1]
+
+### 學習要點總結
+
+-   list of key takeaways
+-   list of key takeaways
+-   list of key takeaways
+
+### 感謝的事
+
+-   list of the things I am grateful for from my day
+-   list of the things I am grateful for from my day
+
+### 今日建議總結
+
+[As a life mentor provide insights into my life]
+
+"""
